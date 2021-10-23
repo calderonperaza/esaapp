21046151c149e03b038bbf8c08b4061031fba8e4
Alex Agreda <agredalex1997@gmail.com>
2021-10-23 09:02:30 -0600

    refactor: change recipient address for contact us email

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ sendButton.addEventListener('click', () => {
 
     Email.send({
       SecureToken: 'e83b8515-0368-4326-9df9-85721ffb762d',
-      To: 'agredalex1997@gmail.com',
+      To: 'calderonperaza@gmail.com',
       From: emailInput.value,
       Subject: `Mensaje de ${nameInput.value} desde esaapp.com`,
       Body: somethingInput.value,
