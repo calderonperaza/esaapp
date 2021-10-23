179264e331d74607300886c7aebd878e95f096bc
Alex Agreda <agredalex1997@gmail.com>
2021-10-23 08:59:20 -0600

    refactor: ux improvements for contact us email

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@ const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const somethingInput = document.getElementById('something');
 
+function clearInputs() {
+  nameInput.value = '';
+  emailInput.value = '';
+  somethingInput.value = '';
+}
+
 sendButton.addEventListener('click', () => {
   if (
     !nameInput.value.trim() ||
@@ -11,14 +17,25 @@ sendButton.addEventListener('click', () => {
   ) {
     alert('Debes llenar todos los campos para poder contactarnos');
   } else {
+    sendButton.innerText = 'Enviando...';
+
     Email.send({
       SecureToken: 'e83b8515-0368-4326-9df9-85721ffb762d',
       To: 'agredalex1997@gmail.com',
       From: emailInput.value,
       Subject: `Mensaje de ${nameInput.value} desde esaapp.com`,
       Body: somethingInput.value,
-    }).then(function () {
-      alert('Pronto nos contactaremos contigo');
-    });
+    })
+      .then(() => {
+        clearInputs();
+        alert('Pronto nos contactaremos contigo');
+      })
+      .catch((error) => {
+        alert('Ocurrió un error. Intente de nuevo más tarde');
+        console.error(error);
+      })
+      .finally(() => {
+        sendButton.innerText = 'ENVIAR';
+      });
   }
 });
